3feaf40d75326dd9bfdb712a00c15270c4bca55d
André <andre_mp_@hotmail.com>
2020-03-12 15:41:21 -0300

    leitura de QR code funcionando

--- app/src/main/java/br/com/polenflorestal/qrcodepolen/MainActivity.kt
+++ app/src/main/java/br/com/polenflorestal/qrcodepolen/MainActivity.kt
@@ -1,47 +1,189 @@
 package br.com.polenflorestal.qrcodepolen
 
-import `in`.mrasif.libs.easyqr.EasyQR
-import `in`.mrasif.libs.easyqr.QRScanner
+import android.Manifest
+import android.app.AlertDialog
+import android.content.DialogInterface
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
+import android.content.pm.PackageManager
 import android.os.Bundle
 import android.view.View
 import android.widget.Button
+import android.widget.ImageButton
 import android.widget.TextView
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
+
 
 class MainActivity : AppCompatActivity() {
 
-    private lateinit var tvData : TextView
+    private lateinit var tvSelected : TextView
     private lateinit var btnQRScan : Button
+    val REQUEST_ID_MULTIPLE_PERMISSIONS = 7
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        tvData = findViewById(R.id.tvData)
+        tvSelected = findViewById(R.id.tvSelected)
         btnQRScan = findViewById(R.id.btnQRScan)
     }
 
-    fun qrScan(view: View) {
+    fun btnScan(view: View) {
+
+        if( checkAndRequestPermissions() == true ){
+            scanQR()
+        }
+    }
+
+    private fun scanQR() {
+
         val intent = Intent(this, QRScanner::class.java)
-        intent.putExtra(EasyQR.CAMERA_BORDER,100);
-        intent.putExtra(EasyQR.CAMERA_BORDER_COLOR,"#C1000000");
-        intent.putExtra(EasyQR.IS_SCAN_BAR,true);
-        intent.putExtra(EasyQR.IS_BEEP,true);
-        intent.putExtra(EasyQR.BEEP_RESOURCE_ID,R.raw.beep);
-        startActivityForResult(intent, EasyQR.QR_SCANNER_REQUEST)
+
+        intent.putExtra(QR_IS_TOOLBAR_SHOW,true);
+        //intent.putExtra(QR_TOOLBAR_DRAWABLE_ID,R.drawable.ic_audiotrack_dark);
+        //intent.putExtra(QR_TOOLBAR_TEXT,"My QR");
+        //intent.putExtra(QR_TOOLBAR_BACKGROUND_COLOR,"#0588EE");
+        //intent.putExtra(QR_TOOLBAR_TEXT_COLOR,"#FFFFFF");
+        intent.putExtra(QR_BACKGROUND_COLOR,"#000000");
+        //intent.putExtra(QR_CAMERA_MARGIN_LEFT,50);
+        //intent.putExtra(QR_CAMERA_MARGIN_TOP,50);
+        //intent.putExtra(QR_CAMERA_MARGIN_RIGHT,50);
+        //intent.putExtra(QR_CAMERA_MARGIN_BOTTOM,50);
+        intent.putExtra(QR_CAMERA_BORDER,50);
+        intent.putExtra(QR_CAMERA_BORDER_COLOR,"#C1000000");
+        intent.putExtra(QR_IS_SCAN_BAR,true);
+        intent.putExtra(QR_IS_BEEP,true);
+        intent.putExtra(QR_BEEP_RESOURCE_ID,R.raw.beep);
+
+
+        startActivityForResult(intent, QR_QR_SCANNER_REQUEST)
+
+    }
+
+    private fun checkAndRequestPermissions(): Boolean {
+        val camera = ContextCompat.checkSelfPermission(this, Manifest.permission.CAMERA)
+        val wtite = ContextCompat.checkSelfPermission(this, Manifest.permission.WRITE_EXTERNAL_STORAGE)
+        val read = ContextCompat.checkSelfPermission(this, Manifest.permission.READ_EXTERNAL_STORAGE)
+
+        val listPermissionsNeeded: MutableList<String> = ArrayList()
+        if (wtite != PackageManager.PERMISSION_GRANTED) {
+            listPermissionsNeeded.add(Manifest.permission.WRITE_EXTERNAL_STORAGE)
+        }
+        if (camera != PackageManager.PERMISSION_GRANTED) {
+            listPermissionsNeeded.add(Manifest.permission.CAMERA)
+        }
+        if (read != PackageManager.PERMISSION_GRANTED) {
+            listPermissionsNeeded.add(Manifest.permission.READ_EXTERNAL_STORAGE)
+        }
+        if (listPermissionsNeeded.isNotEmpty()) {
+            ActivityCompat.requestPermissions(
+                this,
+                listPermissionsNeeded.toTypedArray(),
+                REQUEST_ID_MULTIPLE_PERMISSIONS
+            )
+            return false
+        }
+        return true
+    }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<String>, grantResults: IntArray
+    ) {
+        when (requestCode) {
+            REQUEST_ID_MULTIPLE_PERMISSIONS -> {
+                val perms: MutableMap<String, Int> = HashMap()
+                // Initialize the map with both permissions
+                perms[Manifest.permission.WRITE_EXTERNAL_STORAGE] =
+                    PackageManager.PERMISSION_GRANTED
+                perms[Manifest.permission.CAMERA] = PackageManager.PERMISSION_GRANTED
+                perms[Manifest.permission.READ_EXTERNAL_STORAGE] = PackageManager.PERMISSION_GRANTED
+                // Fill with actual results from user
+                if (grantResults.size > 0) {
+                    var i = 0
+                    while (i < permissions.size) {
+                        perms[permissions[i]] = grantResults[i]
+                        i++
+                    }
+                    // Check for both permissions
+                    if (perms[Manifest.permission.WRITE_EXTERNAL_STORAGE] == PackageManager.PERMISSION_GRANTED && perms[Manifest.permission.CAMERA] == PackageManager.PERMISSION_GRANTED && perms[Manifest.permission.READ_EXTERNAL_STORAGE] == PackageManager.PERMISSION_GRANTED
+                    ) {
+                        scanQR()
+                        // process the normal flow
+//else any one or both the permissions are not granted
+                    } else {
+                        //permission is denied (this is the first time, when "never ask again" is not checked) so ask again explaining the usage of permission
+//                        // shouldShowRequestPermissionRationale will return true
+//show the dialog or snackbar saying its necessary and try again otherwise proceed with setup.
+                        if (ActivityCompat.shouldShowRequestPermissionRationale(
+                                this,
+                                Manifest.permission.WRITE_EXTERNAL_STORAGE
+                            ) || ActivityCompat.shouldShowRequestPermissionRationale(
+                                 this,
+                                Manifest.permission.CAMERA
+                            ) || ActivityCompat.shouldShowRequestPermissionRationale(
+                                this,
+                                Manifest.permission.READ_EXTERNAL_STORAGE
+                            )
+                        ) {
+                            showDialogOK("Camera and Storage Permission required for this app",
+                                DialogInterface.OnClickListener { dialog, which ->
+                                    when (which) {
+                                        DialogInterface.BUTTON_POSITIVE -> checkAndRequestPermissions()
+                                        DialogInterface.BUTTON_NEGATIVE -> {
+                                        }
+                                    }
+                                })
+                        } else {
+                            Toast.makeText(
+                                this,
+                                "Go to settings and enable permissions",
+                                Toast.LENGTH_LONG
+                            )
+                                .show()
+                            //                            //proceed with logic by disabling the related features or quit the app.
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+    private fun showDialogOK(
+        message: String,
+        okListener: DialogInterface.OnClickListener
+    ) {
+        AlertDialog.Builder(this)
+            .setMessage(message)
+            .setPositiveButton("OK", okListener)
+            .setNegativeButton("Cancel", okListener)
+            .create()
+            .show()
     }
 
     override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
         super.onActivityResult(requestCode, resultCode, data)
 
-        when {
-            requestCode == EasyQR.QR_SCANNER_REQUEST -> {
-                if (resultCode == RESULT_OK) {
-                    tvData.text = data?.getStringExtra(EasyQR.DATA) ?: "-1";
-                }
+        if (requestCode == QR_QR_SCANNER_REQUEST){
+            if (resultCode == RESULT_OK) {
+                tvSelected.text = data?.getStringExtra(QR_DATA) ?: "-1";
             }
         }
+
+    }
+
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+
+        outState.putString("selectedID", tvSelected.text.toString())
+    }
+
+    override fun onRestoreInstanceState(savedInstanceState: Bundle) {
+        super.onRestoreInstanceState(savedInstanceState)
+
+        tvSelected.text = savedInstanceState.getString("selectedID", "-1")
     }
 
 }
