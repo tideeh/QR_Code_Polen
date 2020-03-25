ee5d06b63a84dc4968a244325d4a808278dad4bc
André <andre_mp_@hotmail.com>
2020-03-25 02:01:12 -0300

    adicionado ao firebase e ativado crashlight

--- app/src/main/java/br/com/polenflorestal/qrcodepolen/MainActivity.kt
+++ app/src/main/java/br/com/polenflorestal/qrcodepolen/MainActivity.kt
@@ -8,6 +8,7 @@ import android.content.pm.PackageManager
 import android.os.Bundle
 import android.view.MenuItem
 import android.view.View
+import android.view.ViewGroup
 import android.widget.Button
 import android.widget.ImageButton
 import android.widget.TextView
